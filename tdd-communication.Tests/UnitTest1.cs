508909b31140b834c1c8724d26700fedbd05bc42
Charlie Lashmar <charlie@lashmar.com>
2024-11-19 10:26:10 +0000

    completed task 1

--- tdd-communication.Tests/UnitTest1.cs
+++ tdd-communication.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using FluentAssertions;
+
 namespace tdd_communication.Tests
+ 
 {
     public class Tests
     {
@@ -8,9 +11,18 @@ namespace tdd_communication.Tests
         }
 
         [Test]
-        public void Test1()
+        [TestCase(new int[] { }, TestName = "Return 0 when passed an empty string.")]
+        public void SumMiddleNumbersTest_1(int[] array)
         {
-            Assert.Pass();
+            Kata1.SumMiddleNumbers(array).Should().Be(0);
+        }
+
+        [Test]
+        [TestCase(new int[] { 2, 1, 3, 4 }, 5, TestName = "Return 5 when passed 2, 1, 3, 4.")]
+        [TestCase(new int[] { 1 }, 0, TestName = "Return 0 when passed [1].")]
+        public void SumMiddleNumbersTest_2(int[] array, int number)
+        {
+            Kata1.SumMiddleNumbers(array).Should().Be(number);
         }
     }
 }
---
+++ tdd-communication/Kata1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_communication
+{
+    public class Kata1
+    {
+        public static int SumMiddleNumbers(int[] array)
+        {
+            int count = 0;
+
+            Array.Sort(array);
+
+            for(int i = 1; i < array.Length - 1; i++)
+            {
+                count += array[i];
+            }
+
+            return count;
+        }
+    }
+}
