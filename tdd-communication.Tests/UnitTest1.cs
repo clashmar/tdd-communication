f0e174267e89c138f6153bc386648a01c1d9f683
Charlie Lashmar <charlie@lashmar.com>
2024-11-19 10:01:27 +0000

    Add project files.

---
+++ tdd-communication.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace tdd_communication.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
